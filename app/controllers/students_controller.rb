fca484b7c789b673588ed82b09c2411b2e14856a
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-28 06:34:02 -0600

    done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.order(:desc)
+        render json: students
+    end
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+
+    def highest_grade
+        highest_student = Student.order(grade: :desc).first
+        render json: highest_student
+    
+    end
+end
